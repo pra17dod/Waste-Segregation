9aba5760581921ecdacc9cbfcf4b430485e014ac
pra17dod <pra17dod@gmail.com>
2021-03-13 17:50:30 +0530

    Add model.py

---
+++ model.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import shutil
+import re
+import random
+import seaborn as sns
+import cv2 as cv
+from PIL import Image
+import pickle
+import sys
+sys.setrecursionlimit(1000000000)
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+# from tfhub import load_model_weights
+
+## get a path to the folder with images
+path = os.path.join(os.getcwd(),"dataset-resized")
+
+train = tf.keras.preprocessing.image_dataset_from_directory(path, batch_size=16, image_size=(224,224), shuffle=True, label_mode='categorical', validation_split = 0.25, seed = 1, subset = 'training')
+valid = tf.keras.preprocessing.image_dataset_from_directory(path, batch_size=16, image_size=(224,224), shuffle=True, label_mode='categorical', validation_split = 0.25, seed = 1, subset = 'validation')
+
+model = tf.keras.applications.ResNet50(include_top=False, input_shape=(224,224,3), weights = None)
+
+model2 = tf.keras.Sequential()
+model2.add(model)
+model2.add(tf.keras.layers.Flatten())
+model2.add(tf.keras.layers.Dropout(0.2))
+model2.add(Dense(512, activation = 'relu'))
+model2.add(tf.keras.layers.Dropout(0.2))
+model2.add(Dense(256, activation = 'relu'))
+model2.add(tf.keras.layers.Dropout(0.2))
+model2.add(Dense(128, activation = 'relu'))
+model2.add(tf.keras.layers.Dropout(0.2))
+model2.add(Dense(64, activation = 'relu'))
+model2.add(tf.keras.layers.Dropout(0.2))
+model2.add(tf.keras.layers.Dense(7, activation='softmax'))
+
+
+model2.load_weights('./model/weights/waste_weights_heavy.h5')
