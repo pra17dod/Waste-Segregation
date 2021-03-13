e7ce8615f10293611b1792e1e8cc9c18bd0b0daf
pra17dod <pra17dod@gmail.com>
2021-03-14 02:14:37 +0530

    Add working app.py

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 from model import get_output
 from PIL import Image
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 @app.route("/")
 def home():
-    return jsonify(name="What's your name??")
+    return jsonify(name="Hello Welcome to Waste Segregation!!")
 
 @app.route('/predict', methods=['POST'])
 def predict():
