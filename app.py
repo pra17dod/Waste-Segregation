bb30dc24b290ae342df455581f963deff52bd85e
pra17dod <pra17dod@gmail.com>
2021-03-14 01:35:13 +0530

    Add test method in app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 from model import get_output
+from PIL import Image
+from flask_ngrok import run_with_ngrok
+
 app = Flask(__name__)
+run_with_ngrok(app)
 
 @app.route("/")
 def home():
-    return 
+    return jsonify(name="What's your name??")
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return  get_output()
- 
+    path = request.json['image']    
+    prediction = get_output(path) 
+    return jsonify(predict = prediction)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
