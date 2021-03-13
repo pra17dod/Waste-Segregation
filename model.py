6304879e60bcff6b2236ca82680344a8092e86d9
pra17dod <pra17dod@gmail.com>
2021-03-14 01:35:34 +0530

    Update model.py

--- model.py
+++ model.py
@@ -9,6 +9,8 @@ import seaborn as sns
 import cv2 as cv
 from PIL import Image
 import sys
+from PIL import Image
+import urllib.request
 
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
@@ -17,24 +19,16 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 # from tfhub import load_model_weights
 
 
-model = tf.keras.applications.ResNet50(include_top=False, input_shape=(224,224,3), weights = None)
+# model = tf.keras.applications.ResNet50(include_top=False, input_shape=(224,224,3))
 
-model2 = tf.keras.Sequential()
-model2.add(model)
-model2.add(tf.keras.layers.Flatten())
-model2.add(tf.keras.layers.Dropout(0.2))
-model2.add(Dense(512, activation = 'relu'))
-model2.add(tf.keras.layers.Dropout(0.2))
-model2.add(Dense(256, activation = 'relu'))
-model2.add(tf.keras.layers.Dropout(0.2))
-model2.add(Dense(128, activation = 'relu'))
-model2.add(tf.keras.layers.Dropout(0.2))
-model2.add(Dense(64, activation = 'relu'))
-model2.add(tf.keras.layers.Dropout(0.2))
-model2.add(tf.keras.layers.Dense(7, activation='softmax'))
+# model.trainable = False
+# model2 = tf.keras.Sequential()
+# model2.add(model)
+# model2.add(tf.keras.layers.Flatten())
+# model2.add(tf.keras.layers.Dropout(0.2))
+# model2.add(tf.keras.layers.Dense(6, activation='softmax'))
 
-
-model2.load_weights('./model/weights/waste_weights_heavy.h5')
+model2 = tf.keras.models.load_model('./model/model.h5')
 
 ## get a path to the folder with images
 # path = os.path.join(os.getcwd(),"model/dataset-resized")
@@ -49,13 +43,27 @@ class_name = { 0:'Biodegradable:Reuseable Cardboard waste', 1:'Non-Biodegradable
 def get_names(cache):
     return class_name[cache]
 
-def get_output(path):
+def get_output(url):
 
-    temp = img_to_array(load_img(path, target_size=(224,224))).reshape(1,224,224,3)
+    img = Image.open(urllib.request.urlopen(url))
+    temp = img_to_array(img)
+    # print(temp.shape)
+    temp = tf.image.resize(temp, [224,224])
+    # print(temp.shape)
+    temp = tf.reshape(temp, [-1,224,224,3])
     cache = np.argmax(model2.predict(temp))
     prediction = get_names(cache)
+    
     return prediction
 
+if __name__ == "__main__":
+    print(get_output(sys.argv[1]))
+if __name__ == "__main__":
+    print(get_output(sys.argv[1]))
+if __name__ == "__main__":
+    print(get_output(sys.argv[1]))
+if __name__ == "__main__":
+    print(get_output(sys.argv[1]))
 if __name__ == "__main__":
     print(get_output(sys.argv[1]))
 
