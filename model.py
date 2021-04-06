3bb09fca9c358aba629742fb1d262b01123f5b55
pra17dod <pra17dod@gmail.com>
2021-04-06 18:11:11 +0530

    model.py: Supress tensorflow log

--- model.py
+++ model.py
@@ -1,6 +1,13 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# 0 = all messages are logged (default behavior)
+# 1 = INFO messages are not printed
+# 2 = INFO and WARNING messages are not printed
+# 3 = INFO, WARNING, and ERROR messages are not printed
 import urllib.request
 import sys
 import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
 
 model2 = tf.keras.models.load_model('./model/model_final.h5')
 
@@ -15,7 +22,6 @@ def get_output(url):
 
     img = urllib.request.urlopen(url).read()
     temp = tf.io.decode_jpeg(img, channels = 3)
-    # temp = img_to_array(img)
     temp = tf.image.resize(temp, [224,224])
     temp = tf.reshape(temp, [-1,224,224,3])
     cache = tf.keras.backend.eval(tf.math.argmax(model2.predict(temp), axis = -1))[0]
